a5be111071319227b099a2a70d1f451ba90bf752
Wagner Lopes <wagnerlopesbr@hotmail.com>
2023-09-11 10:15:03 -0300

    WL - Project RTL 2/7 v1

--- src/tests/About.test.tsx
+++ src/tests/About.test.tsx
@@ -1,1 +1,15 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+const imgSrc = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+
+describe('Testando o arquivo About.tsx', () => {
+  test('Teste se a página contém as informações sobre a Pokédex', () => {
+    renderWithRouter(<App />, { route: '/about' });
+    const aboutHeading = screen.getByRole('heading', { name: /About Pokédex/i });
+    const aboutImage = screen.getByRole('img');
+    expect(aboutHeading).toBeInTheDocument();
+    expect(aboutImage).toHaveAttribute('src', imgSrc);
+  });
+});
