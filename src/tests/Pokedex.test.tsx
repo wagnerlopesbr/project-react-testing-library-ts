00e1e03edd55fc9dd44bc68623dfe7772da14a1b
Wagner Lopes <wagnerlopesbr@hotmail.com>
2023-09-11 13:31:55 -0300

    WL - Project RTL 5/7 v1

--- src/tests/Pokedex.test.tsx
+++ src/tests/Pokedex.test.tsx
@@ -1,1 +1,64 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import pokemonList from '../data';
+import App from '../App';
+
+describe('Testando o arquivo Pokedex.tsx', () => {
+  test('Teste se a página contém um heading h2 com o texto Encountered Pokémon', () => {
+    renderWithRouter(<App />, { route: '/' });
+    expect(screen.getByText(/Encountered Pokémon/i)).toBeInTheDocument();
+  });
+
+  test('Teste se é exibido o próximo Pokémon da lista quando o botão Próximo Pokémon é clicado', async () => {
+    const { user } = renderWithRouter(<App />, { route: '/' });
+    let nextButtonClickCount = 0;
+    const pokemon = screen.getByTestId('pokemon-name');
+    const nextPokemonButton = screen.getByRole('button', { name: /Próximo pokémon/i });
+    expect(pokemon).toHaveTextContent(pokemonList[0].name);
+    await user.click(nextPokemonButton);
+    nextButtonClickCount++;
+    expect(pokemon).toHaveTextContent(pokemonList[1].name);
+    await user.click(nextPokemonButton);
+    nextButtonClickCount++;
+    expect(pokemon).toHaveTextContent(pokemonList[2].name);
+    if (nextButtonClickCount === pokemonList.length) {
+      expect(pokemon).toHaveTextContent(pokemonList[0].name);
+    }
+  });
+
+  test('Teste se é mostrado apenas um Pokémon por vez', () => {
+    renderWithRouter(<App />, { route: '/' });
+    const pokemon = screen.getAllByTestId('pokemon-name');
+    expect(pokemon.length).toBe(1);
+  });
+
+  test('Teste se a Pokédex tem os botões de filtro', () => {
+    renderWithRouter(<App />, { route: '/' });
+    const buttons = screen.getAllByTestId('pokemon-type-button');
+    buttons.forEach((button, pokeType) => {
+      const typesList = pokemonList.map((pokemon) => pokemon.type);
+      typesList.forEach((type) => {
+        if (!type) {
+          screen.getByText(type);
+        }
+      });
+      button.textContent = `${typesList[pokeType]}`;
+      expect(button).toHaveTextContent(typesList[pokeType]);
+    });
+
+    // expect(buttons.length).toBe(7);
+    // expect(buttons[0]).toHaveTextContent(/Electric/i);
+    // expect(buttons[1]).toHaveTextContent(/Fire/i);
+    // expect(buttons[2]).toHaveTextContent(/Bug/i);
+    // expect(buttons[3]).toHaveTextContent(/Poison/i);
+    // expect(buttons[4]).toHaveTextContent(/Psychic/i);
+    // expect(buttons[5]).toHaveTextContent(/Normal/i);
+    // expect(buttons[6]).toHaveTextContent(/Dragon/i);
+  });
+
+  test('Teste se a Pokédex contém um botão para resetar o filtro', () => {
+    renderWithRouter(<App />, { route: '/' });
+    const resetButton = screen.getByRole('button', { name: /All/i });
+    expect(resetButton).toBeInTheDocument();
+  });
+});
