eb59dca22dae066c7d0d5bf3349ec442a5e45a32
Wagner Lopes <wagnerlopesbr@hotmail.com>
2023-09-11 15:44:05 -0300

    WL - Project RTL 5/7 v8

--- src/tests/Pokedex.test.tsx
+++ src/tests/Pokedex.test.tsx
@@ -3,6 +3,8 @@ import renderWithRouter from '../renderWithRouter';
 import pokemonList from '../data';
 import App from '../App';
 
+const pokeTest = 'pokemon-name';
+
 describe('Testando o arquivo Pokedex.tsx', () => {
   test('Teste se a página contém um heading h2 com o texto Encountered Pokémon', () => {
     renderWithRouter(<App />, { route: '/' });
@@ -11,7 +13,7 @@ describe('Testando o arquivo Pokedex.tsx', () => {
 
   test('Teste se é exibido o próximo Pokémon da lista quando o botão Próximo Pokémon é clicado', async () => {
     const { user } = renderWithRouter(<App />, { route: '/' });
-    const pokemon = screen.getByTestId('pokemon-name');
+    const pokemon = screen.getByTestId(pokeTest);
     const nextPokemonButton = screen.getByRole('button', { name: /Próximo pokémon/i });
 
     expect(pokemon).toHaveTextContent(pokemonList[0].name);
@@ -37,7 +39,7 @@ describe('Testando o arquivo Pokedex.tsx', () => {
 
   test('Teste se é mostrado apenas um Pokémon por vez', () => {
     renderWithRouter(<App />, { route: '/' });
-    const pokemon = screen.getAllByTestId('pokemon-name');
+    const pokemon = screen.getAllByTestId(pokeTest);
     expect(pokemon.length).toBe(1);
   });
 
@@ -57,16 +59,19 @@ describe('Testando o arquivo Pokedex.tsx', () => {
     const { user } = renderWithRouter(<App />, { route: '/' });
     const buttons = screen.getAllByTestId('pokemon-type-button');
     const allButton = screen.getByRole('button', { name: 'All' });
-    const fireButton = buttons[0];
-    const bugButton = buttons[1];
+    const fireButton = buttons[1];
+    const bugButton = buttons[2];
     expect(allButton).toBeInTheDocument();
     await user.click(fireButton);
-    const pokemon = screen.getByTestId('pokemon-name');
+    const pokemon = screen.getByTestId(pokeTest);
     const nextButton = screen.getByRole('button', { name: 'Próximo Pokémon' });
     expect(pokemon).toHaveTextContent('Charmander');
     expect(allButton).toBeInTheDocument();
     await user.click(nextButton);
     expect(pokemon).toHaveTextContent('Rapidash');
+    expect(allButton).toBeInTheDocument();
+    await user.click(nextButton);
+    expect(pokemon).toHaveTextContent('Charmander');
     expect(allButton).toBeInTheDocument();
     await user.click(bugButton);
     expect(pokemon).toHaveTextContent('Caterpie');
