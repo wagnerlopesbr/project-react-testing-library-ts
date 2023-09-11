0af4aa7871c4488ca92ca57237845f8d2b520f3b
Wagner Lopes <wagnerlopesbr@hotmail.com>
2023-09-11 12:30:09 -0300

    WL - Project RTL 4/7 v1

--- src/tests/NotFound.test.tsx
+++ src/tests/NotFound.test.tsx
@@ -1,1 +1,16 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testando o arquivo NotFound.tsx', () => {
+  test('Teste se a página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<App />, { route: '/GABIGOL' });
+    expect(screen.getByText(/Page requested not found/i)).toBeInTheDocument();
+  });
+
+  test('Teste se a página mostra a imagem com o texto alternativo https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    renderWithRouter(<App />, { route: '/GABIGOL' });
+    const notFoundImgAltText = screen.getByAltText(/Clefairy pushing buttons randomly with text I have no idea what i'm doing/i);
+    expect(notFoundImgAltText).toBeInTheDocument();
+  });
+});
